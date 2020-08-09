b7ed4b79447e1788c8ea781967218036334d8084
jairomartinez1994 <61437470+jairomartinez1994@users.noreply.github.com>
2020-08-09 13:36:48 -0400

    update

--- client/src/pages/AddYourHome.js
+++ client/src/pages/AddYourHome.js
@@ -13,21 +13,25 @@ import {Jumbotron, Button, Form} from "react-bootstrap";
                 </p>
                 <Form>
                 <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Label>Name</Form.Label>
-    <Form.Control type="Name" placeholder="First and Last Name" />
+    <Form.Label>First Name</Form.Label>
+    <Form.Control type="Name" placeholder="First Name" />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Last Name</Form.Label>
+    <Form.Control type="Name" placeholder="Last Name" />
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect1">
-    <Form.Label>Example select</Form.Label>
+    <Form.Label>Location</Form.Label>
     <Form.Control as="select">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
+      <option>Jersey City</option>
+      <option>New York City</option>
+      <option>Newark</option>
+      <option>Piscataway</option>
+      <option>New Brunswick</option>
     </Form.Control>
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect2">
-    <Form.Label>Example multiple select</Form.Label>
+    <Form.Label>Select Number of Guests</Form.Label>
     <Form.Control as="select" multiple>
       <option>1</option>
       <option>2</option>
@@ -35,15 +39,13 @@ import {Jumbotron, Button, Form} from "react-bootstrap";
       <option>4</option>
       <option>5</option>
     </Form.Control>
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Example textarea</Form.Label>
-    <Form.Control as="textarea" rows="3" />
   </Form.Group> 
-             </Form> 
+             </Form>
+             <Button variant="success">Add Your Home</Button>{' '} 
                 </Jumbotron>
                 </div>
             </div>
+
         );
     
     }
