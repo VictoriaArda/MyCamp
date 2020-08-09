a13f114fff0f1b513baa0c470d71357f7867883b
sarita87das <nutankarna87@gmail.com>
2020-08-09 10:53:43 -0400

    placed search result in a row and columns

--- client/src/components/Files/Searchbar.js
+++ client/src/components/Files/Searchbar.js
@@ -3,16 +3,14 @@ import "../Style/searchbar.css";
 import axios from 'axios';
 import { Consumer } from "../../context";
 import Daterange from "./Daterange";
-
-
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {Row,Col} from "react-bootstrap"
 class Searchbar extends Component {
     state = {
         location: "",
         guests: "",
         listings: []
     };
-
     searchLocation = (e) => {
         e.preventDefault();
         axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${this.state.location}&key=4f4e2ab63ee1486aa8d4376d737c5ce1&language=en&pretty=1`)
@@ -31,83 +29,71 @@ class Searchbar extends Component {
                         "check_out": "2021-02-26",
                     }
                 })
-
             })
-
             .then(res => {
                 console.log(res.data)
                 return this.setState({ listings: res.data.listings });
-
             })
             .catch(err => console.log(Error));
-
-
-
     }
-
-
-
-
     onChange = (e) => {
         this.setState({ location: e.target.value });
     }
     onChange2 = (e) => {
         this.setState({ guests: e.target.value });
     }
-
     render() {
         return (
             <Consumer>
                 {value => {
                     return (
-                        <div className="card card-body mb-4 p-4">
-                            <h1 className="display-4 text-center">Search Locations</h1>
-                            <Daterange></Daterange>
-                            <form onSubmit={this.searchLocation}>
-                                <div className="form-group">
-                                    <input
-                                        type="text" className="form-control form-control-lg" placeholder="Location..."
-                                        name="location"
-                                        value={this.state.location}
-                                        onChange={this.onChange} />
-                                    <input
-                                        type="text" className="form-control form-control-lg" placeholder="# of Guests"
-                                        name="guests"
-                                        value={this.state.guests}
-                                        onChange={this.onChange2} />
-
-                                </div>
-                                <button className="btn btn-primary btn-lg btn-block mb-5" type="submit">Search Locations</button>
-                            </form>
-                            
-                            {//replace the div here with a call to your component to display a listing. make the listing available as a prop to that component.
-                            //you also need to assign a key to each component, that should be unique. Something like a id or a name that comes back in the listing.
-                            }
-                            <div className="img-container"></div>
-
-                            {this.state.listings.map((listing) => { return <div key={listing.listing.id}> <img className="image" src={listing.listing.picture_url} alt={listing.listing.picture_url}/><br/>
-                            {listing.pricing_quote.rate.amount_formatted}
-                             </div> })}
-                        </div>);
+                        <section className="search-bar">
+                            <Row className="container">
+                                <form className ="row" onSubmit={this.searchLocation}>
+                                <Daterange />
+                                    <div className="form-group col">
+                                        <input
+                                            type="text" className="form-control" placeholder="Location..."
+                                            name="location"
+                                            value={this.state.location}
+                                            onChange={this.onChange} />
+                                    </div>
+                                    <div className="col">
+                                        <input
+                                            type="text" className="form-control" placeholder="# of Guests"
+                                            name="guests"
+                                            value={this.state.guests}
+                                            onChange={this.onChange2} />
+                                    </div>
+                                    <div className="col">
+                                        <button className="btn btn-primary" type="submit">Search</button>
+                                    </div>
+                                </form>
+                                {//replace the div here with a call to your component to display a listing. make the listing available as a prop to that component.
+                                    //you also need to assign a key to each component, that should be unique. Something like a id or a name that comes back in the listing.
+                                }
+                               <Row>
+                                {this.state.listings.map((listing) => {
+                                    return <Col sm={3} xs={3} lg={3} md={3}>
+                                        <div className="card mb-4 shadow-sm">
+                                            <div className="card-body">
+                                                <h3>{listing.listing.city}</h3>
+                                                <img className="image" src={listing.listing.picture_url} alt={listing.picture_url} />
+                                                <p className="card-text">
+                                                    <strong> Price: {listing.pricing_quote.rate.amount_formatted} a night </strong>
+                                                </p>
+                                                <Link to={`path='/Payment'${listing.listing.id}`} className="btn btn-primary" > View Listing
+                                                 </Link>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                })}
+                                </Row>
+                            </Row>
+                        </section>);
                 }}
-
             </Consumer>
-
         )
     }
 }
-
 export default Searchbar;
-// export default Searchbar;<div className="col-md-6">
-//             <div className="card mb-4 shadow-sm">
-//                 <div className="card-body">
-//                 <h3>{listing.city}</h3>
-//                 <img className="image"src={listing.picture_url} alt={listing.picture_url}/>
-//                 <p className="card-text">
-//                     <strong> Price: </strong>
-//                 </p>
-//                 <Link to={`view/listing/${listing.id}`} className="btn btn-primary" > View Listing
-//                 </Link>
-//                 </div>
-//             </div>
-//         </div>
